3f07c8ec6382cbe1884a43c44f590e87f09f4b86
rolandito-cpu <58791898+rolandito-cpu@users.noreply.github.com>
2019-12-11 18:55:27 -0300

    Add files via upload

---
+++ ProyectoTriviaRolandoGimenez/js/datos_preguntas.js
@@ -0,0 +1,252 @@
+/*
+Archivo: datos_preguntas.js
+Configuración de las preguntas del juego.
+*/
+
+let preguntas = [
+  {
+   texto_pregunta:'¿Que podemos reciclar?',
+    opciones:[
+      'Botellas de plastico',
+      'Yerba mate usada',
+      'Cartones mojados'
+    ],
+    imagen_src:'img/preguntas/img_preg_01.png',
+    respuesta_correcta: 'Botellas de plastico'
+  },
+  {
+   texto_pregunta:'El vidrio puede ser reutilizado',
+    opciones:[
+      'Infinitas veces.',
+      '10 veces.',
+      '248 veces como mucho.'
+    ],
+    imagen_src:'img/preguntas/img_generica.png',
+    respuesta_correcta: 'Infinitas veces.'
+  },
+  {
+   texto_pregunta:'Una manera de frenar el calentamiento global es',
+    opciones:[
+      'Reciclar',
+      'Cambiar nuestro estilo de vida a uno de consumo responsable',
+      'Todas las anteriores'
+    ],
+    imagen_src:'img/preguntas/img_preg_03.png',
+    respuesta_correcta: 'Todas las anteriores'
+  },
+  {
+   texto_pregunta:'¿Que es el calentamiento global?',
+    opciones:[
+      'Globos calientes',
+      'El aumento exponencial de la temperatura terrestre',
+      'Es directamente proporcional al voltaje o tensión del mismo e inversamente proporcional a la resistencia que presenta.'
+    ],
+    imagen_src:'img/preguntas/img_preg_04.png',
+    respuesta_correcta: 'El aumento exponencial de la temperatura terrestre'
+  },
+  {
+   texto_pregunta:'¿Qué son las energías renovables?',
+    opciones:[
+      'Son moléculas formadas por aminoácidos que están unidos por un tipo de enlaces conocidos como enlaces peptídicos.',
+      'Son aquellas fuentes de energía basadas en la utilización de recursos naturales',
+      'Son aquellos principios, virtudes o cualidades que caracterizan a una persona, una acción o un objeto que se consideran típicamente positivos o de gran importancia por un grupo social'
+    ],
+    imagen_src:'img/preguntas/La-inversion-en-energias-renovables-se-ralentiza.jpg',
+    respuesta_correcta:  'Son aquellas fuentes de energía basadas en la utilización de recursos naturales'
+  },
+  {
+   texto_pregunta:'Una caracteristica de las energias renovables es: ',
+    opciones:[
+      'Le quitan el trabajo a la gente',
+      'Contaminan el medio ambiente',
+      'Ayudan a potenciar el consumo'
+    ],
+    imagen_src:'img/preguntas/kisspng-renewable-energy-geothermal-energy-solar-energy-ge-sun-power-svg-library-stock-techflourish-collect-5be29cc8edf7b7.3745839615415779289747.jpg',
+    respuesta_correcta: 'Ayudan a potenciar el consumo'
+  },
+  {
+   texto_pregunta:'Para protejer a los animales podemos',
+    opciones:[
+      'Comprar pieles',
+      'Respetar las areas protegidas y reservas naturales',
+      'Todas las anteriores'
+    ],
+    imagen_src:'img/preguntas/endangered-species-giant-panda-tiger-clip-art-others.jpg',
+    respuesta_correcta: 'Respetar las areas protegidas y reservas naturales'
+  },
+  {
+   texto_pregunta:'¿Que hacer frente a esta situación?',
+    opciones:[
+      'Sacar una foto',
+      'Devolverlo al agua o llamar al guarda costa',
+      'Mirar y asombrarse esperando a que alguien haga algo'
+    ],
+    imagen_src:'img/preguntas/signo1.jpg',
+    respuesta_correcta: 'Devolverlo al agua o llamar al guarda costa'
+  },
+  {
+   texto_pregunta:'La deforestacion puede ocasionarse por: ',
+    opciones:[
+      'El hombre',
+      'Causas naturales',
+      'Todas las anteriores'
+    ],
+    imagen_src:'img/preguntas/deforestacion-elefantes-e-incendios-forestales_1308-33603.jpg',
+    respuesta_correcta: 'Todas las anteriores'
+  },
+  {
+   texto_pregunta:'¿Que es la deforestacion? ',
+    opciones:[
+      'Es un dispositivo informático que es capaz de recibir, almacenar y procesar información de una forma útil.',
+      'Es la manera de terminar con la idealizacion de un mundo ideal en el que el hombre domina.',
+      'Es la eliminación del bosque para convertirlo a un uso no forestal.'
+    ],
+    imagen_src:'img/preguntas/hqdefault.jpg',
+    respuesta_correcta: 'Es la eliminación del bosque para convertirlo a un uso no forestal.'
+  },
+  {
+   texto_pregunta:'¿Quién es el responsable civil por un accidente de tránsito producido por un menor de edad poseedor de una licencia de conductor?',
+    opciones:[
+      'El que lo acompaña.',
+      'El que firmó la autorización para obtener la licencia.',
+      'El que le autorizó el uso del vehículo.'
+    ],
+    imagen_src:'img/preguntas/img_generica.png',
+    respuesta_correcta: 'El que firmó la autorización para obtener la licencia.'
+  },
+  {
+   texto_pregunta:'El que conduce un vehículo ¿qué documentación personal debe llevar consigo?',
+    opciones:[
+      'La licencia de conductor es suficiente.',
+      'La licencia de conductor y su documento de identidad.',
+      'La licencia de conductor, su documento de identidad y la cédula verde.'
+    ],
+    imagen_src:'img/preguntas/img_generica.png',
+    respuesta_correcta: 'La licencia de conductor y su documento de identidad.'
+  },
+  {
+   texto_pregunta:'¿Está permitido estacionar moto vehículos sobre la vereda?',
+    opciones:[
+      'Sólo si la vereda es ancha.',
+      'Únicamente en días feriados.',
+      'No, no está permitido.'
+    ],
+    imagen_src:'img/preguntas/img_generica.png',
+    respuesta_correcta: 'No, no está permitido.'
+  },
+  {
+   texto_pregunta:'¿Qué tipo de casco deben utilizar obligatoriamente todas las personas que circulan en motocicletas o vehículos similares?',
+    opciones:[
+      'Opción A',
+      'Opción B',
+      'Opción C'
+    ],
+    imagen_src:'img/preguntas/img_preg_14.png',
+    respuesta_correcta: 'Opción C'
+  },
+  {
+   texto_pregunta:'¿Qué debe hacer si observa que un vehículo está a punto de alcanzarlo y pasarlo?',
+    opciones:[
+      'Ceñirse bien a la derecha, y si es necesario, bajar la velocidad.',
+      'Aumentar la velocidad.',
+      'Es indiferente la velocidad de su vehículo.'
+    ],
+    imagen_src:'img/preguntas/img_generica.png',
+    respuesta_correcta: 'Ceñirse bien a la derecha, y si es necesario, bajar la velocidad.'
+  },
+  {
+   texto_pregunta:'Si el conductor de un vehículo circula con las balizas encendidas, toca repetidamente la bocina y el acompañante saca el brazo agitando un pañuelo. ¿Qué están indicando?',
+    opciones:[
+      'Que el vehículo tiene un desperfecto.',
+      'Que transportan a una persona grave a un hospital.',
+      'Al exceso de velocidad y a la violación de luz roja.'
+    ],
+    imagen_src:'img/preguntas/img_generica.png',
+    respuesta_correcta: 'Que transportan a una persona grave a un hospital.'
+  },
+  {
+   texto_pregunta:'¿Cuál es el nivel máximo de alcoholemia admitido para conducir un automóvil particular?',
+    opciones:[
+      '0,8 gr. por litro de sangre.',
+      '0,5 gr. por litro de sangre.',
+      '1,5 gr. por litro de sangre.'
+    ],
+    imagen_src:'img/preguntas/img_generica.png',
+    respuesta_correcta: '0,5 gr. por litro de sangre.'
+  },
+  {
+   texto_pregunta:'Ud. está llegando a un cruce de calles sin semáforos ni policía, una persona está cruzando por la senda peatonal, Ud. debe:',
+    opciones:[
+      'Disminuir la marcha y tener cuidado.',
+      'Parar y cederle el paso al peatón.',
+      'Seguir y atravesar el cruce, porque tiene prioridad.'
+    ],
+    imagen_src:'img/preguntas/img_generica.png',
+    respuesta_correcta: 'Parar y cederle el paso al peatón.'
+  },
+  {
+   texto_pregunta:'¿Qué deben hacer los conductores para facilitar el paso a un coche de bomberos en servicio de emergencia?',
+    opciones:[
+      'Aumentar la velocidad para no ser un obstáculo.',
+      'Avisar a otros conductores usando repetidamente la bocina.',
+      'Apartarse normalmente a la derecha y, si es necesario, detenerse.'
+    ],
+    imagen_src:'img/preguntas/img_generica.png',
+    respuesta_correcta: 'Apartarse normalmente a la derecha y, si es necesario, detenerse.'
+  },
+  {
+   texto_pregunta:'¿Cuál es la velocidad máxima permitida en autopistas, en el ámbito de la Ciudad Autónoma de Buenos Aires?',
+    opciones:[
+      '70 km/h.',
+      '100 km/h.',
+      '120 km/h.'
+    ],
+    imagen_src:'img/preguntas/img_generica.png',
+    respuesta_correcta: '100 km/h.'
+  },
+  {
+   texto_pregunta:'¿Según se muestra en la fotografía, donde se debe parar?',
+    opciones:[
+      'El lugar de detención no tiene importancia. Pero hay que parar antes de entrar al cruce.',
+      'Antes de la columna en la cual se encuentra la señal de tránsito.',
+      'Antes y cerca de la línea de detención.'
+    ],
+    imagen_src:'img/preguntas/img_preg_21.png',
+    respuesta_correcta: 'Antes y cerca de la línea de detención.'
+  }
+  ,
+  {
+   texto_pregunta:'¿Cómo se conducirá al acercarse a un cruce donde hay un semáforo donde titila el amarillo?',
+    opciones:[
+      'Acelere la velocidad antes que cambie la luz a roja en el semáforo.',
+      'Reducir la velocidad y dar prioridad de paso al vehículo que viene sólo por la izquierda.',
+      'Reducir la velocidad e incluso parar si es necesario - según el tráfico a su alrededor.'
+    ],
+    imagen_src:'img/preguntas/img_preg_22.png',
+    respuesta_correcta: 'Reducir la velocidad e incluso parar si es necesario - según el tráfico a su alrededor.'
+  }
+  ,
+  {
+   texto_pregunta:'¿Qué vehículo está girando correctamente en la rotonda?',
+    opciones:[
+      'El rojo.',
+      'El celeste.',
+      'El Amarillo.'
+    ],
+    imagen_src:'img/preguntas/img_preg_23.png',
+    respuesta_correcta: 'El celeste.'
+  }
+  ,
+  {
+   texto_pregunta:'Como norma de carácter general, ¿quién tiene prioridad de paso en una encrucijada o bocacalle?',
+    opciones:[
+      'Los vehículos que viene por el lado izquierdo.',
+      'El vehículo que va a girar.',
+      'El vehículo que se presenta por el lado derecho.'
+    ],
+    imagen_src:'img/preguntas/img_preg_24.png',
+    respuesta_correcta: 'El vehículo que se presenta por el lado derecho.'
+  }
+
+];
+
---
+++ ProyectoTriviaRolandoGimenez/js/mi_codigo.js
@@ -0,0 +1,141 @@
+	/* 
+Archivo mi_codigo.js
+En este archivo programaremos el código correspondiente
+al juego de Trivia.
+ */
+
+var indice_pregunta_actual;
+var total_puntos;
+const nombre_alumno = "Rolando";
+const maximo_preguntas_por_jugada = 10;
+const puntos_resultado_bien = 6;
+
+
+
+let mensaje;
+function mostrarResultado(){
+	document.querySelector('#pantalla-juego').classList.add('d-none');
+	document.querySelector('#pantalla-resultado').classList.remove('bien');
+	document.querySelector('#pantalla-resultado').classList.remove('mal');
+	if (total_puntos >= puntos_resultado_bien) {
+		document.querySelector('#pantalla-resultado').classList.add('bien');
+	}else{
+		document.querySelector('#pantalla-resultado').classList.add('mal');
+	}
+	document.querySelector('#resultado-puntos').textContent = total_puntos;
+	document.querySelector('#pantalla-resultado').classList.remove('d-none');
+}
+
+function obtenerSiguientePregunta(){
+	indice_pregunta_actual++;
+	
+	if (indice_pregunta_actual < preguntas.length && indice_pregunta_actual < maximo_preguntas_por_jugada){
+		return preguntas[indice_pregunta_actual];
+	}else{
+
+		return null;
+	}
+}
+
+
+
+
+function verificarPreguntaActual(){
+	let pregunta = preguntas[indice_pregunta_actual];
+	let input_opciones = document.querySelectorAll('input');
+	let recuadro = document.querySelectorAll('#opciones div');
+
+		for(let i = 0; i < input_opciones.length; i++){
+			if (input_opciones[i].value == pregunta.respuesta_correcta){
+				recuadro[i].classList.add('correcta');
+				console.log('correcta');
+				if (input_opciones[i].checked) {
+					total_puntos++;
+				}
+			}else{
+				recuadro[i].classList.add('erronea');
+				console.log('erronea');
+			}
+		}
+}
+
+function manejadorBotonSiguiente(){
+	let pregunta = obtenerSiguientePregunta();
+	if (pregunta != null) {
+		mostrarPregunta(pregunta);
+	}
+	else{
+		mostrarResultado();
+	}
+	document.querySelector('#boton-siguiente').classList.add('d-none');
+	document.querySelector('#boton-verificar').classList.remove('d-none');
+}
+
+
+
+
+
+
+
+
+function manejadorBotonVerificar(){
+
+	let opcion_seleccionada = document.querySelector('input:checked');
+	if (opcion_seleccionada) {
+		verificarPreguntaActual();
+		document.querySelector('#boton-verificar').classList.add('d-none');
+		document.querySelector('#boton-siguiente').classList.remove('d-none');
+	}
+
+
+}
+
+
+
+
+function mostrarPregunta(pregunta){
+	document.querySelector('#pregunta-numero').textContent = (indice_pregunta_actual+1) + ') ';
+	document.querySelector('#pregunta-texto').textContent = pregunta.texto_pregunta;
+	document.querySelector('#pregunta-imagen').src = pregunta.imagen_src;
+	let div_opciones = document.querySelectorAll('#opciones div');
+	let input_opciones = document.querySelectorAll('#opciones input');
+	let label_opciones = document.querySelectorAll('#opciones label');
+	for (let i= 0; i<3 ; i++){
+		div_opciones[i].classList.remove("correcta");
+		div_opciones[i].classList.remove("erronea");
+		input_opciones[i].checked = false;
+		input_opciones[i].value = pregunta.opciones[i];
+		label_opciones[i].textContent = pregunta.opciones[i];
+	}
+}
+
+function iniciarJuego()
+{
+	var elemento1=document.querySelector("#pantalla-inicio");
+	elemento1.classList.add("d-none");
+	var elemento2=document.querySelector("#pantalla-resultado");
+	elemento2.classList.add("d-none"); 
+	var elemento3=document.querySelector("#header");
+	elemento3.classList.remove("d-none");
+	var elemento4=document.querySelector("#pantalla-juego");
+	elemento4.classList.remove("d-none");
+	
+	indice_pregunta_actual = 0;
+	total_puntos = 0;
+
+	mostrarPregunta(preguntas[indice_pregunta_actual]);
+}
+
+let boton_siguiente = document.querySelector('#boton-siguiente');
+boton_siguiente.addEventListener('click', manejadorBotonSiguiente)
+
+let boton_verificar = document.querySelector('#boton-verificar');
+boton_verificar.addEventListener('click', manejadorBotonVerificar);
+
+let idBotonJugar = document.querySelector("#inicio-boton-jugar");
+idBotonJugar.addEventListener("click", iniciarJuego);
+
+let idBotonVolverAJugar = document.querySelector("#resultado-boton-volver-a-jugar");
+idBotonVolverAJugar.addEventListener("click", iniciarJuego);
+
+//document.querySelector(".nombre_alumno").textContent = nombre_alumno
